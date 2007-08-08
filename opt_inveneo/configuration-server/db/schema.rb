c2eecf29e7d25920c0ccae05449d30c87456e715
jwishnie <jwishnie@539d6c29-9a2f-0410-8196-a36328eba1ad>
2007-08-08 23:30:41 +0000

    mongrel_cluster

--- opt_inveneo/configuration-server/db/schema.rb
+++ opt_inveneo/configuration-server/db/schema.rb
@@ -7,8 +7,6 @@ ActiveRecord::Schema.define(:version => 1) do
   create_table "initial_configs", :force => true do |t|
     t.column "mac",                :string,  :default => "NULL"
     t.column "timezone",           :string,  :default => "'America/Los_Angeles'"
-    t.column "hostname",           :string,  :default => "NULL"
-    t.column "hostname_prefix",    :string,  :default => "'inveneo-cs-'"
     t.column "ntp_on",             :boolean, :default => false,                                                 :null => false
     t.column "ntp_servers",        :string,  :default => "'pool.ntp.org'",                                      :null => false
     t.column "proxy_on",           :boolean, :default => false,                                                 :null => false
