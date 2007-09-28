58bcedefaae03c98b6bcb35f5226080174b3b242
jwishnie <jwishnie@539d6c29-9a2f-0410-8196-a36328eba1ad>
2007-09-28 23:26:18 +0000

    check for arrays that are already active

--- opt_inveneo/sbin/inv_check_superblocks.sh
+++ opt_inveneo/sbin/inv_check_superblocks.sh
@@ -10,6 +10,12 @@ part_exists() {
 # args are: array-device device1 device2
 # e.g. check_raid /dev/md0 /dev/sdb1 /dev/sdb2
 check_raid() { 
+    if mdadm --detail --brief --test $1
+	then
+	echo "RAID array at $1 already active"
+	return
+    fi
+
     # try to examine superblocks
     mdadm -E $2 2> /dev/null
     DRIVE1_SUPERBLOCK_OK=$?
@@ -19,43 +25,44 @@ check_raid() {
 
     if  [ $DRIVE1_SUPERBLOCK_OK -eq 0 ] &&  [ $DRIVE2_SUPERBLOCK_OK -eq 0 ]
 	then 
-	echo "RAID at $1: OK"
+	echo "RAID array at $1 has superblocks. Leaving along"
+	return
+    fi
+    
+    # superblock is gone. Check if drives in array exist
+    if  part_exists $2 
+	then
+	DRIVE1=$2
     else
-	# superblock is gone. Check if drives in array exist
-	if  part_exists $2 
+	echo "$2 not found, marking as 'missing'"
+	DRIVE1="missing"
+    fi
+    if  part_exists $3 
+	then
+	DRIVE2=$3
+    else
+	echo "$3 not found, marking as 'missing'"
+	DRIVE2="missing"
+    fi
+    
+    # see if we have the drive present that has the problem to bother rewriting
+    SHOULD_REWRITE=1 # false
+    if [ ! $DRIVE1_SUPERBLOCK_OK -eq 0 ] && [ $DRIVE1 != "missing" ]
+	then
+	SHOULD_REWRITE=0
+    elif [ ! $DRIVE2_SUPERBLOCK_OK -eq 0 ] && [ $DRIVE2 != "missing" ]
+	then
+	SHOULD_REWRITE=0
+    fi
+    
+    if [ $SHOULD_REWRITE -eq 0 ]
+	then
+	echo "Attempting to rewrite super-blocks for DEVICES: $DRIVE1,$DRIVE2 on ARRAY: $1"
+	yes | mdadm --create $1 --level=1 --raid-devices=2 --auto=yes  $DRIVE1 $DRIVE2
+	if [ $? -eq 0 ]
 	    then
-	    DRIVE1=$2
-	else
-	    echo "$2 not found, marking as 'missing'"
-	    DRIVE1="missing"
-	fi
-	if  part_exists $3 
-	    then
-	    DRIVE2=$3
-	else
-	    echo "$3 not found, marking as 'missing'"
-	    DRIVE2="missing"
-	fi
-	
-	# see if we have the drive present that has the problem to bother rewriting
-	SHOULD_REWRITE=1 # false
-	if [ ! $DRIVE1_SUPERBLOCK_OK -eq 0 ] && [ $DRIVE1 != "missing" ]
-	    then
-	    SHOULD_REWRITE=0
-	elif [ ! $DRIVE2_SUPERBLOCK_OK -eq 0 ] && [ $DRIVE2 != "missing" ]
-	    then
-	    SHOULD_REWRITE=0
-	fi
-
-	if [ $SHOULD_REWRITE -eq 0 ]
-	    then
-	    echo "Attempting to rewrite super-blocks for DEVICES: $DRIVE1,$DRIVE2 on ARRAY: $1"
-	    yes | mdadm --create $1 --level=1 --raid-devices=2 --auto=yes  $DRIVE1 $DRIVE2
-	    if [ $? -eq 0 ]
-		then
-		echo "stopping newly created array"
-		mdadm --stop $1
-	    fi
+	    echo "stopping newly created array"
+	    mdadm --stop $1
 	fi
     fi
 }
