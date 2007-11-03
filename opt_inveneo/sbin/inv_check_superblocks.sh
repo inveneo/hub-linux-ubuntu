3a49d9da237284b5a420b3ddc910bddb533c29fe
jwishnie <jwishnie@539d6c29-9a2f-0410-8196-a36328eba1ad>
2007-11-03 00:36:54 +0000

    simplified logic and only attempt to rewrite superblock on one drive of an array

--- opt_inveneo/sbin/inv_check_superblocks.sh
+++ opt_inveneo/sbin/inv_check_superblocks.sh
@@ -16,62 +16,65 @@ part_exists() {
 check_raid() { 
     mdadm --detail --brief --test $1
     if [ $? -lt 2 ]
-	then
-	echo "RAID array at $1 already active"
-	return
+    then
+        echo "RAID array at $1 already active"
+        return
     fi
 
-    # try to examine superblocks
-    mdadm -E $2 2> /dev/null
-    DRIVE1_SUPERBLOCK_OK=$?
-
-    mdadm -E $3 2> /dev/null
-    DRIVE2_SUPERBLOCK_OK=$?
-
-    if  [ $DRIVE1_SUPERBLOCK_OK -eq 0 ] &&  [ $DRIVE2_SUPERBLOCK_OK -eq 0 ]
-	then 
-	echo "RAID array at $1 has superblocks. Leaving along"
-	return
-    fi
-    
-    # superblock is gone. Check if drives in array exist
+    # see if partitions are present
     if  part_exists $2 
-	then
-	DRIVE1=$2
+    then
+        DRIVE1=$2
     else
-	echo "$2 not found, marking as 'missing'"
-	DRIVE1="missing"
+        echo "$2 not found, marking as 'missing'"
+        DRIVE1="missing"
     fi
+    
     if  part_exists $3 
-	then
-	DRIVE2=$3
-    else
-	echo "$3 not found, marking as 'missing'"
-	DRIVE2="missing"
+    then
+        DRIVE2=$3
+        else
+        echo "$3 not found, marking as 'missing'"
+        DRIVE2="missing"
     fi
     
-    # see if we have the drive present that has the problem to bother rewriting
-    SHOULD_REWRITE=1 # false
-    if [ ! $DRIVE1_SUPERBLOCK_OK -eq 0 ] && [ $DRIVE1 != "missing" ]
-	then
-	SHOULD_REWRITE=0
-    elif [ ! $DRIVE2_SUPERBLOCK_OK -eq 0 ] && [ $DRIVE2 != "missing" ]
-	then
-	SHOULD_REWRITE=0
+    # if all drives missing, nothing to do
+    if [ $DRIVE1 -eq "missing" ] && [ $DRIVE2 -eq "missing" ]
+    then
+        echo "Neither $2 nor $3 found, nothing to do for array $1"
+        return
     fi
     
-    if [ $SHOULD_REWRITE -eq 0 ]
-	then
-	echo "Attempting to rewrite super-blocks for DEVICES: $DRIVE1,$DRIVE2 on ARRAY: $1"
-	yes | mdadm --create $1 --size=max --level=1 --raid-devices=2 --auto=yes  $DRIVE1 $DRIVE2
-	if [ $? -eq 0 ]
-	    then
-	    echo "stopping newly created array"
-	    mdadm --stop $1
-	fi
-    else
-	echo "All present drives have superblocks. Nothing to fix"
+    # try to examine superblocks
+    DRIVE1_SUPERBLOCK_OK=0
+    if [ "$DRIVE1" -ne "missing" ]
+    then
+        mdadm -E $DRIVE1 2> /dev/null
+        DRIVE1_SUPERBLOCK_OK=$?
     fi
+
+    DRIVE2_SUPERBLOCK_OK=0
+    if [ "$DRIVE2" -ne "missing" ]
+    then
+        mdadm -E $DRIVE2 2> /dev/null
+        DRIVE2_SUPERBLOCK_OK=$?
+    fi
+
+    if  [ $DRIVE1_SUPERBLOCK_OK -eq 0 ] ||  [ $DRIVE2_SUPERBLOCK_OK -eq 0 ]
+    then 
+        echo "RAID array at $1 has superblocks on at least one drive. Leaving along"
+        return
+    fi
+    
+    # Ok, if we got here we have at _least_ one drive present and no superblock
+    echo "Attempting to rewrite super-blocks for DEVICE: $DRIVE1 $DRIVE2 on ARRAY: $1"
+    yes | mdadm --create $1 --size=max --level=1 --raid-devices=2 --auto=yes  $DRIVE1 $DRIVE2
+    if [ $? -eq 0 ]
+        then
+        echo "stopping newly created array"
+        mdadm --stop $1
+    fi
+
 }
 
 
@@ -95,13 +98,13 @@ awk '/^ARRAY/ {
     printf("%s ", $2) 
     for (i = 2; i <= NF; i++) 
     if ( split($i, devices_arg,"=") > 0 && 
-	    devices_arg[1] == "devices" ) { 
-	    num_devs = split(devices_arg[2], devices, ",") 
-	    for ( j = 1; j <= num_devs; j++ ) { 
-		printf("%s ", devices[j]) 
-	    } 
-	} 
-	printf("\n") } ' $CONF | while read line; do
+        devices_arg[1] == "devices" ) { 
+        num_devs = split(devices_arg[2], devices, ",") 
+        for ( j = 1; j <= num_devs; j++ ) { 
+        printf("%s ", devices[j]) 
+        } 
+    } 
+    printf("\n") } ' $CONF | while read line; do
     check_raid $line
 done
 
