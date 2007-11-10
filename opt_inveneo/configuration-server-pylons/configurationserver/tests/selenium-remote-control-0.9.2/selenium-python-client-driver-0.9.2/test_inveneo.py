f261e033d5f96e4c48c342f9aca5d1205ac09227
rjocham <rjocham@539d6c29-9a2f-0410-8196-a36328eba1ad>
2007-11-10 01:11:59 +0000

    'fixed

--- opt_inveneo/configuration-server-pylons/configurationserver/tests/selenium-remote-control-0.9.2/selenium-python-client-driver-0.9.2/test_inveneo.py
+++ opt_inveneo/configuration-server-pylons/configurationserver/tests/selenium-remote-control-0.9.2/selenium-python-client-driver-0.9.2/test_inveneo.py
@@ -1,10 +1,11 @@
 from selenium import selenium
 import unittest
+import string
 
 class TestConfigurationServer(unittest.TestCase):
     def setUp(self):
         self.selenium = selenium("localhost", \
-            4444, "*firefox", "http://localhost:8008")
+            4444, "*firefox", "http://192.168.15.198:8008")
         self.selenium.start()
         self.selenium.open("/admin/config_remove/000000000000")
         
@@ -44,19 +45,25 @@ class TestConfigurationServer(unittest.TestCase):
         sel.click("commit")
         sel.wait_for_page_to_load("30000")
         sel.open("/admin/edit/000000000000")
-        self.assertEqual("000000000000", sel.get_value("mac"))
+        text = sel.get_body_text()
+	self.assertTrue(string.find(text,"000000000000") > 0) 
         sel.open("/admin/config_remove/000000000000")
         
     def test_should_proof_that_mac_are_unique(self):
-    	import string
     	sel = self.selenium
         sel.open("/admin/config_add")
         sel.type("mac", "000000000000")
+        sel.type("http_proxy_port", "8080")
+        sel.type("https_proxy_port", "8888")
+        sel.type("ftp_proxy_port", "4000")
         sel.type("locale", "en_US.UTF-8")
         sel.click("commit")
         sel.wait_for_page_to_load("30000")
         sel.open("/admin/config_add")
         sel.type("mac", "000000000000")
+        sel.type("http_proxy_port", "8080")
+        sel.type("https_proxy_port", "8888")
+        sel.type("ftp_proxy_port", "4000")
         sel.type("locale", "en_US.UTF-8")
         sel.click("commit")
         sel.wait_for_page_to_load("30000")
@@ -65,7 +72,6 @@ class TestConfigurationServer(unittest.TestCase):
         sel.open("/admin/config_remove/000000000000")
 
     def test_should_proof_that_mac_are_12_lowercase_hex(self):
-    	import string
     	sel = self.selenium
         sel.open("/admin/config_add")
         sel.type("mac", "zzzzyyyywwww")
@@ -75,7 +81,6 @@ class TestConfigurationServer(unittest.TestCase):
         self.assertTrue(string.find(text,"MAC address must be 12 hex lower case values, no separator") > 0) 
 
     def test_should_proof_that_locale_complies_to_standard(self):
-    	import string
     	sel = self.selenium
         sel.open("/admin/config_add")
         sel.type("locale", "Here")
@@ -84,17 +89,12 @@ class TestConfigurationServer(unittest.TestCase):
         text = sel.get_body_text()
         self.assertTrue(string.find(text,"Must be a valid locale string. E.g. en_UK.utf-8") > 0) 
 
-    def test_should_proof_that_locale_complies_to_standard(self):
-    	import string
-    	sel = self.selenium
-        sel.open("/configuration/get_user_config/ralph")
-        
     def test_should_proof_that_there_is_always_a_deaddeadbeef_mac(self):
     	sel = self.selenium
         sel.open("/admin/edit/deaddeadbeef")
         sel.wait_for_page_to_load("2500")
-        self.assertEqual("deaddeadbeef", sel.get_value("mac"))    	    	
-            
+        text = sel.get_body_text()
+	self.assertTrue(string.find(text,"deaddeadbeef") > 0)     
     def tearDown(self):
         self.selenium.stop()
 
