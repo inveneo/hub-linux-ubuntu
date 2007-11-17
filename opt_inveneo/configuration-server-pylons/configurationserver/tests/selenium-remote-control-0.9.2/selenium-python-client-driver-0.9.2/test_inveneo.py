19a6fddad68d49db5381d9add7b00d7f2f1735e9
rjocham <rjocham@539d6c29-9a2f-0410-8196-a36328eba1ad>
2007-11-17 00:10:51 +0000

    'added

--- opt_inveneo/configuration-server-pylons/configurationserver/tests/selenium-remote-control-0.9.2/selenium-python-client-driver-0.9.2/test_inveneo.py
+++ opt_inveneo/configuration-server-pylons/configurationserver/tests/selenium-remote-control-0.9.2/selenium-python-client-driver-0.9.2/test_inveneo.py
@@ -1,13 +1,18 @@
 from selenium import selenium
 import unittest
+import os
 import string
 import httplib
+import time
 
 PROTOCOL = "http"
-URL = "192.168.15.198"
+URL = "192.168.1.104"
 PORT = "8008"
 
 class TestConfigurationServer(unittest.TestCase):
+    def assertStringInBodyText(self, str_txt):
+    	self.assertTrue(string.find(self.selenium.get_body_text(), str_txt) >= 0)
+
     def setUp(self):
         self.selenium = selenium("localhost", \
             4444, "*firefox", PROTOCOL + "://" + URL + ":" + PORT)
@@ -17,7 +22,17 @@ class TestConfigurationServer(unittest.TestCase):
 	self.selenium.type("password", "1nvene0")
 	self.selenium.click("commit")
         self.selenium.wait_for_page_to_load("30000")
-        self.selenium.open("/admin/config_remove/000000000000")        	        
+        self.selenium.open("/admin/config_remove/000000000000")        	
+        self.selenium.open("/admin/station_remove/111111111111")
+        
+    def test_wrong_loging_should_show_error_message(self):
+       	sel = self.selenium
+        sel.open("/signin/signin")
+	sel.type("username", "wrong")
+	sel.type("password", "worse")
+	sel.click("commit")
+        sel.wait_for_page_to_load("30000")
+        self.assertStringInBodyText("Username and password combination are not valid.")
         
     def test_signout_should_redirect_to_signin_and_stay(self):
        	sel = self.selenium
@@ -28,6 +43,18 @@ class TestConfigurationServer(unittest.TestCase):
 	sel.open("/admin/dashboard")
         sel.wait_for_page_to_load("30000")
         self.assertTrue(string.find(sel.get_location(), "/signin/signin") >= 0)
+        
+    def test_up_and_download_station_config(self):
+        sel = self.selenium
+        sel.open("/admin/set_server_on/1?on=True")
+        sel.wait_for_page_to_load("30000")
+    	os.system('curl --fail -s -w %{size_upload} -F config_file=@def_station_config.tar.gz http://' + URL + ':' + PORT + '/configuration/save_station_config/111111111111 -v')
+        sel.click("//input[@value='List Station Configurations']")
+        sel.wait_for_page_to_load("30000")
+        self.assertStringInBodyText("111111111111")        
+        os.system('curl -O http://' + URL + ':' + PORT + '/configuration/get_station_config/111111111111')
+	self.assertEquals(os.stat('def_station_config.tar.gz')[6], os.stat('111111111111')[6])
+	os.remove('111111111111')
         
     def test_get_501_on_get_config_when_server_is_off(self):   	
        	sel = self.selenium
@@ -41,15 +68,15 @@ class TestConfigurationServer(unittest.TestCase):
     	conn.request("GET", "/configuration/get_user_config/any_name_will_do")
     	req = conn.getresponse()
     	conn.close()
-    	self.assertTrue(501, req.status)
+    	self.assertEquals(501, req.status)
     	conn.request("GET", "/configuration/get_station_config/any_mac_will_do")
 	req = conn.getresponse()
 	conn.close()
-	self.assertTrue(501, req.status)
+	self.assertEquals(501, req.status)
     	conn.request("GET", "/configuration/get_station_initial_config/any_mac_will_do")
 	req = conn.getresponse()
 	conn.close()
-	self.assertTrue(501, req.status)
+	self.assertEquals(501, req.status)
 	
 	sel.open("/admin/dashboard")
 	sel.wait_for_page_to_load("2500")
@@ -69,15 +96,15 @@ class TestConfigurationServer(unittest.TestCase):
     	conn.request("GET", "/configuration/save_user_config/any_name_will_do")
     	req = conn.getresponse()
     	conn.close()
-    	self.assertTrue(501, req.status)
+    	self.assertEquals(404, req.status)
     	conn.request("GET", "/configuration/save_station_config/any_mac_will_do")
 	req = conn.getresponse()
 	conn.close()
-	self.assertTrue(501, req.status)
+	self.assertEquals(404, req.status)
     	conn.request("GET", "/configuration/save_station_initial_config/any_mac_will_do")
 	req = conn.getresponse()
 	conn.close()
-	self.assertTrue(501, req.status)
+	self.assertEquals(404, req.status)
 	
 	sel.open("/admin/dashboard")
 	sel.wait_for_page_to_load("2500")
@@ -89,7 +116,9 @@ class TestConfigurationServer(unittest.TestCase):
         sel = self.selenium
         sel.open("/")  
         sel.wait_for_page_to_load(30000)
-        self.assertTrue(string.find(sel.get_location(), "/admin/dashboard") >= 0)
+	time.sleep(2)        
+        location = sel.get_location()
+        self.assertTrue(string.find(location, "/admin/dashboard") >= 0)
         
     def test_click_list_configurations_shows_list_site(self):
     	sel = self.selenium
@@ -121,8 +150,7 @@ class TestConfigurationServer(unittest.TestCase):
         sel.click("commit")
         sel.wait_for_page_to_load("30000")
         sel.open("/admin/edit/000000000000")
-        text = sel.get_body_text()
-	self.assertTrue(string.find(text,"000000000000") > 0) 
+        self.assertStringInBodyText("000000000000")
         sel.open("/admin/config_remove/000000000000")
         
     def test_should_proof_that_mac_are_unique(self):
@@ -143,8 +171,7 @@ class TestConfigurationServer(unittest.TestCase):
         sel.type("locale", "en_US.UTF-8")
         sel.click("commit")
         sel.wait_for_page_to_load("30000")
-        text = sel.get_body_text()
-        self.assertTrue(string.find(text,"MAC is already being used") > 0) 
+        self.assertStringInBodyText("MAC is already being used")
         sel.open("/admin/config_remove/000000000000")
 
     def test_should_proof_that_mac_are_12_lowercase_hex(self):
@@ -153,8 +180,7 @@ class TestConfigurationServer(unittest.TestCase):
         sel.type("mac", "zzzzyyyywwww")
         sel.click("commit")
         sel.wait_for_page_to_load("30000")
-        text = sel.get_body_text()
-        self.assertTrue(string.find(text,"MAC address must be 12 hex lower case values, no separator") > 0) 
+        self.assertStringInBodyText("MAC address must be 12 hex lower case values, no separator")
 
 # right now locale comes from a combo box
 #
@@ -169,17 +195,27 @@ class TestConfigurationServer(unittest.TestCase):
 
     def test_switch_server_on_off(self):
     	sel = self.selenium
+        sel.open("/admin/set_server_on/1?on=True")
+        sel.wait_for_page_to_load("30000")
+        self.assertStringInBodyText("YES")
+        sel.open("/admin/set_server_on/1?on=False")
+        sel.wait_for_page_to_load("30000")
+        self.assertStringInBodyText("NO")
+        sel.open("/admin/set_server_on/1?on=True")
+        sel.wait_for_page_to_load("30000")
+        self.assertStringInBodyText("YES")
+
+    def test_toggle_server_on_off(self):
+    	sel = self.selenium
         sel.open("/admin/dashboard")
         sel.wait_for_page_to_load("2500")
-        text = sel.get_body_text()
-        if (string.find(text, "YES") > 0):
+        if string.find(sel.get_body_text(), "YES"):
             expected = "NO"
         else:
             expected = "YES"
 	sel.click("//input[@value='Toggle']")
         sel.wait_for_page_to_load("30000")
-        text = sel.get_body_text()
-	self.assertTrue(string.find(text,expected) > 0)     
+	self.assertStringInBodyText(expected)     
 	
     def test_swich_all_stations_on_off(self):
     	sel = self.selenium
@@ -199,9 +235,8 @@ class TestConfigurationServer(unittest.TestCase):
     	sel = self.selenium
         sel.open("/admin/edit/deaddeadbeef")
         sel.wait_for_page_to_load("2500")
-        text = sel.get_body_text()
-	self.assertTrue(string.find(text,"deaddeadbeef") > 0)     
-    
+        self.assertStringInBodyText("deaddeadbeef")
+        
     def tearDown(self):
         self.selenium.stop()
 
