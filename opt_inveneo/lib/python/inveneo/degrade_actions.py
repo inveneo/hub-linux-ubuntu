92fd6772199f292d58d4b060d68b8580eeaca94c
tw_josh <tw_josh@539d6c29-9a2f-0410-8196-a36328eba1ad>
2008-02-28 21:11:02 +0000

    degrade_actions loop checks to see if the disks have come back online before performing actions. If they have, exit. Altered get_disk_status and update_disk_status to be callable from other routines.

--- opt_inveneo/lib/python/inveneo/degrade_actions.py
+++ opt_inveneo/lib/python/inveneo/degrade_actions.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 
-import notify_icip
 import os
 import time
 import sys
 import constants
+import notify_icip
+import get_disk_status
+import update_disk_status
 
 def main(disk=None):
+
 	while True:
+
+		# check if the array is restored
+		old_disks = get_disk_status.main()
+		new_disks = update_disk_status.main()
+		if (new_disks >= old_disks):
+			return
+
+		# INSERT YOUR ACTIONS BELOW
+		
 		os.system(constants.INV_MONITOR_BEEP_ALERT)
 		notify_icip.send_notice()
+		
+		# INSERT YOUR ACTIONS ABOVE
+
+		# sleep for some minutes
 		time.sleep(constants.INV_MONITOR_DEGRADE_ACTIONS_SLEEP_MINUTES*60)
+
+	# end while
 
 
 if __name__ == '__main__':
--- opt_inveneo/lib/python/inveneo/update_disk_status.py
+++ opt_inveneo/lib/python/inveneo/update_disk_status.py
@@ -3,6 +3,7 @@
 from __future__ import with_statement
 
 import os
+import sys
 import re
 
 # Needs to run as root or sudo
@@ -11,30 +12,37 @@ import re
 
 DISK_STATUS_FILE='/etc/inveneo/disk_status_count'
 
-earlier_disks = -1
+def main():
+    earlier_disks = -1
 
-if os.path.exists(DISK_STATUS_FILE):
-    with open(DISK_STATUS_FILE) as f:
-        earlier_disks = int(f.read())
+    if os.path.exists(DISK_STATUS_FILE):
+        with open(DISK_STATUS_FILE) as f:
+            earlier_disks = int(f.read())
 
-output = ''
-try:
-    mdadm_command = os.popen('mdadm --detail /dev/md0')
-    output = mdadm_command.read()
-finally:
+    output = ''
     try:
-        mdadm_command.close()
-    except:
-        pass
+        mdadm_command = os.popen('mdadm --detail /dev/md0')
+        output = mdadm_command.read()
+    finally:
+        try:
+            mdadm_command.close()
+        except:
+            pass
 
-find_working = re.compile(r'Working Devices\s+:\s+(\d)')
-result = find_working.findall(output)
+    find_working = re.compile(r'Working Devices\s+:\s+(\d)')
+    result = find_working.findall(output)
 
-detected_disks = -1
+    detected_disks = -1
 
-if result:
-    detected_disks = int(result[0])
+    if result:
+        detected_disks = int(result[0])
 
-if (earlier_disks < detected_disks):
-    with open(DISK_STATUS_FILE, 'w') as f:
-        print >>f, detected_disks
+    if (earlier_disks < detected_disks):
+        with open(DISK_STATUS_FILE, 'w') as f:
+            print >>f, detected_disks
+
+    return detected_disks
+
+if __name__ == '__main__':
+	number_of_disks = main()
+	sys.exit()
