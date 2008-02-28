2c41db0d8b506d4cf2b5fa1fe64ce94eecef6891
tw_sammy <tw_sammy@539d6c29-9a2f-0410-8196-a36328eba1ad>
2008-02-28 03:43:14 +0000

    initial revision of the mail notification module

---
+++ opt_inveneo/lib/python/inveneo/notify_icip.py
@@ -0,0 +1,35 @@
+import smtplib
+import socket
+import sys
+
+SMTP_USERNAME='inveneo.smtp@gmail.com'
+SMTP_PASSWORD='1qaz2wsx'
+SMTP_HOSTNAME='smtp.gmail.com'
+SMTP_PORT=587
+SMTP_TLS=1
+SMTP_SENDER='inveneo.smtp@gmail.com'
+SMTP_RECIPIENT='sammy.zahabi@gmail.com' #icip address
+SMTP_DEFAULT_MESSAGE="Host has a failed disk drive:"
+SMTP_DEFAULT_SUBJECT="Subject: Disk failed"
+
+def send_notice(subject=SMTP_DEFAULT_SUBJECT, message=SMTP_DEFAULT_MESSAGE + " " + socket.gethostname()):
+#	message = SMTP_DEFAULT_MESSAGE + str(socket.gethostname)
+# get the email message from a file
+	message = subject + "\n\n" + message
+	try:
+		s = smtplib.SMTP(SMTP_HOSTNAME, SMTP_PORT)
+		s.set_debuglevel(1)
+		s.ehlo()
+		s.starttls()
+		s.ehlo()
+		s.login(SMTP_USERNAME, SMTP_PASSWORD)
+		s.sendmail(SMTP_SENDER, SMTP_RECIPIENT, message)
+		s.noop()
+		s.rset()
+		s.quit()
+		s.close()
+	except Exception, e:
+		print "network down"
+
+send_notice()
+sys.exit()
