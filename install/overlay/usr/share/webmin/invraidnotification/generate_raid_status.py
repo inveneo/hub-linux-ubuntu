388548a0ef7906291f04450aa9a0fa583203efd9
lnelson <lnelson@539d6c29-9a2f-0410-8196-a36328eba1ad>
2008-03-19 01:13:16 +0000

    added error handling code for get_drive_num

--- install/overlay/usr/share/webmin/invraidnotification/generate_raid_status.py
+++ install/overlay/usr/share/webmin/invraidnotification/generate_raid_status.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 import sys
+import string
 sys.path.append('/opt/inveneo/lib/python/inveneo')
 import fileutils, raidutils, diskutils, constants
 
 def get_drive_number(config,serial_num,num_expected_drives):
-    for drive_num in range(num_expected_drives):
-        if config.get_as_str("DISK%d" % drive_num) == serial_num:
+    if serial_num == None: 
+        return -1 
+
+    for drive_num in range(num_expected_drives+1):
+        config_serial = config.get_as_str("DISK%d" % drive_num)
+        if not config_serial:
+                continue
+        elif config_serial.lower().strip() == serial_num.lower().strip():
                 return drive_num
+
     return -1
 
 def main():
@@ -33,6 +41,7 @@ def main():
 
         for logical_name,drive_state in drives_in_array:
                 serial_num=diskutils.id_for_device('/dev/%s' % logical_name)
+                serial_num='def456'
                 drive_num=get_drive_number(config,serial_num,num_expected)
                 all_drive_information.append( (drive_num, logical_name, serial_num, drive_state) )
 
@@ -51,7 +60,6 @@ def main():
                 else:
                     print "<td><table><tr><td>Drive Number: %d</td></tr><tr><td>Logical Name: %s</td></tr><tr><td>Serial: %s</td></tr><tr><td>State: %s</td></tr></table></td>" % (drive_num, logical_name, serial_num, drive_state)
         print "</tr></table>"
-
 
 if __name__ == "__main__":
         main()
