9b0839021ec985dcff430b1a938bfd7b7851eff9
jen <jen@539d6c29-9a2f-0410-8196-a36328eba1ad>
2011-02-10 00:01:01 +0000

    reverting to old file

--- install/bin/configure_openvpn.sh
+++ install/bin/configure_openvpn.sh
@@ -7,5 +7,5 @@
 source /etc/openvpn/easy-rsa/vars
 /etc/openvpn/easy-rsa/clean-all
 /etc/openvpn/easy-rsa/build-dh
-/etc/openvpn/easy-rsa/pkitool --initca
-/etc/openvpn/easy-rsa/pkitool --server inveneo-hub
+#/etc/openvpn/easy-rsa/pkitool --csr HOSTNAME
+
